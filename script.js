b33ef644db1d854bd93c5237d28a35bde49cd4ec
Anders Lindberg <anderslindberg999@gmail.com>
2023-08-30 13:08:00 +0200

    first.commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const list = document.querySelector("ul");
+
+const numSojler = 20;
+
+const intervalTime = 1000;
+
+const myArray = [];
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateListItem(numCustomers) {
+  const li = document.createElement("li");
+  const randomHeight = (numCustomers / 100) * 100;
+  li.style.setProperty("--height", randomHeight);
+
+  list.appendChild(li);
+
+  if (list.children.length > numSojler) {
+    list.removeChild(list.firstElementChild);
+  }
+}
+
+function generateAndPush() {
+  const numCustomers = generateRandomNumber(0, 100);
+  myArray.push(numCustomers);
+  generateListItem(numCustomers);
+}
+const intervalId = setInterval(generateAndPush, intervalTime);
